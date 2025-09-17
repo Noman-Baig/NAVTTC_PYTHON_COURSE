b981c533cb52c40a1cdf979c1f8cad35a43be548
Mirza Noman Baig <98812536+Noman-Baig@users.noreply.github.com>
2025-09-17 14:05:32 +0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,30 @@
+# ===============================
+# VARIABLES IN PYTHON - NOTES
+# ===============================
+
+name = "Ali"        # string
+age = 25            # integer
+height = 5.9        # float
+is_student = True   # boolean
+
+
+string_var = "Python"
+int_var = 42
+float_var = 3.14
+bool_var = False
+list_var = [1, 2, 3]
+tuple_var = (1, 2, 3)
+dict_var = {"key": "value"}
+set_var = {1, 2, 3}
+
+# 6. Constants (by convention, use UPPERCASE)
+
+PI = 3.14159
+MAX_USERS = 100
+
+# 7. Checking Type
+
+print(type(name))     # <class 'str'>
+print(type(age))      # <class 'int'>
+
+# ===============================
