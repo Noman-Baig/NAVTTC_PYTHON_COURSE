2e3387af11f04bdaff564ce4d855ba76a237082d
Mirza Noman Baig <98812536+Noman-Baig@users.noreply.github.com>
2025-09-19 12:43:48 +0500

    Create loop_practice.py

---
+++ loop_practice.py
@@ -0,0 +1,27 @@
+# Looping with range
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+for i in range(len(fruits)):
+    print(i, fruits[i])
+
+Output:
+
+0 apple
+1 banana
+2 mango
+3 orange
+
+# Looping without range
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+for fruit in fruits:
+    print(fruit)
+
+Output:
+
+apple
+banana
+mango
+orange
