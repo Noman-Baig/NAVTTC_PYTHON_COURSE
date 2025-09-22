9b7863619e138874e363fcd8258b8c1055ebde53
Mirza Noman Baig <98812536+Noman-Baig@users.noreply.github.com>
2025-09-22 13:21:19 +0500

    Create if_else_elif.py

---
+++ if_else_elif.py
@@ -0,0 +1,20 @@
+if condition1:
+   
+elif condition2:
+   
+elif condition3:
+   
+else:
+
+# Simple Example
+
+age = 20
+
+if age < 13:
+    print("Child")
+elif age < 18:
+    print("Teenager")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior Citizen")
