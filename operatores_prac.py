bcaa32ea0c4c2dfda85cea76d40d62c38a75b8f6
Mirza Noman Baig <98812536+Noman-Baig@users.noreply.github.com>
2025-09-22 13:28:59 +0500

    Create operatores_prac.py

---
+++ operatores_prac.py
@@ -0,0 +1,34 @@
+# 1. Arithmetic Operators
+
+print(a + b)   # (Addition)
+print(a - b)   # (Subtraction)
+print(a * b)   # (Multiplication)
+print(a / b)   # (Division)
+print(a // b)  # (Floor Division)
+print(a % b)   # (Modulus - remainder)
+print(a ** b)  # (Exponentiation: 10^3)
+
+# 2. Comparison (Relational) Operators
+
+print(x == y)  # False (equal to)
+print(x != y)  # True  (not equal to)
+print(x > y)   # If x greater then y then True
+print(x < y)   # If x greater then y then False
+print(x >= y)  # If x greater or equal to y then True
+print(x <= y)  # If x less or equal to y then True
+
+# 3. Logical Operators
+
+print(a and b)  # (both must be True)
+print(a or b)   # (at least one True)
+print(not a)    # (negation)
+
+# 4. Assignment Operators
+
+x += 3   # x = x + 3
+x -= 2   # x = x - 2
+x *= 4   # x = x * 4
+x /= 6   # x = x / 6
+x %= 3   # x = x % 3
+x **= 2  # x = x ** 2
+x //= 2  # x = x // 2
